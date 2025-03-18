31ca54e77475e2f5579537065dcfc8ec07f38158
Nompumelelo <akilaprofvictoria@gmail.com>
2025-03-18 11:50:39 +0200

    urls

--- index.js
+++ index.js
@@ -10,13 +10,20 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/images", express.static(path.join(__dirname, "public/images"))); 
+// Serve static images correctly
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 // Routes
 app.use("/api/recipes", require("./routes/recipes"));
 
 // Root route
 app.get("/", (req, res) => {
   res.send("Foodify API is running!");
+});
+
+// Catch-All for Unknown Routes
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
 // Start server
